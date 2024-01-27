b294390680dde43b4b866a9dfd86821ae49e412f
blackviking991 <ishaanrock138@gmail.com>
2024-01-27 20:59:52 +0530

    added routing and leader board component

--- frontend/T2T/src/app/app.routes.ts
+++ frontend/T2T/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { ForumPageComponent } from './components/forum-page/forum-page.component';
+import { CreatePostComponent } from './components/create-post/create-post.component';
+import { ProfilePageComponent } from './components/profile-page/profile-page.component';
+export const routes: Routes = [
+    {path: 'home', component: HomePageComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: 'forums', component: ForumPageComponent},
+    {path: 'profile', component: ProfilePageComponent},
+    {path: 'create-new', component: CreatePostComponent},
 
-export const routes: Routes = [];
+];
