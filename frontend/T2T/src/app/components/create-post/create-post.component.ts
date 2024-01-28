16bb051ff5d2a5a206a552dc93d11a738c979d10
blackviking991 <ishaanrock138@gmail.com>
2024-01-28 18:57:20 +0530

    removed editor

--- frontend/T2T/src/app/components/create-post/create-post.component.ts
+++ frontend/T2T/src/app/components/create-post/create-post.component.ts
@@ -1,46 +1,46 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { Validators, Editor, Toolbar, NgxEditorModule } from 'ngx-editor';
+// import { Validators, Editor, Toolbar, NgxEditorModule } from 'ngx-editor';
 
 import jsonDoc from './doc';
 @Component({
   selector: 'app-create-post',
   standalone: true,
-  imports: [NgxEditorModule, ReactiveFormsModule],
+  imports: [ ReactiveFormsModule],
   templateUrl: './create-post.component.html',
   styleUrl: './create-post.component.scss'
 })
-export class CreatePostComponent implements OnInit, OnDestroy {
-  editordoc = jsonDoc;
+export class CreatePostComponent {
+  // editordoc = jsonDoc;
 
-  editor!: Editor;
-  toolbar: Toolbar = [
-    ['bold', 'italic'],
-    ['underline', 'strike'],
-    ['code', 'blockquote'],
-    ['ordered_list', 'bullet_list'],
-    [{ heading: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }],
-    ['link', 'image'],
-    ['text_color', 'background_color'],
-    ['align_left', 'align_center', 'align_right', 'align_justify'],
-  ];
+  // editor!: Editor;
+  // toolbar: Toolbar = [
+  //   ['bold', 'italic'],
+  //   ['underline', 'strike'],
+  //   ['code', 'blockquote'],
+  //   ['ordered_list', 'bullet_list'],
+  //   [{ heading: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }],
+  //   ['link', 'image'],
+  //   ['text_color', 'background_color'],
+  //   ['align_left', 'align_center', 'align_right', 'align_justify'],
+  // ];
 
-  Editorform = new FormGroup({
-    editorContent: new FormControl(
-      { value: jsonDoc, disabled: false },
-      Validators.required()
-    ),
-  });
+  // Editorform = new FormGroup({
+  //   editorContent: new FormControl(
+  //     { value: jsonDoc, disabled: false },
+  //     Validators.required()
+  //   ),
+  // });
 
-  // get doc(): AbstractControl {
-  //   return this.Editorform.get('editorContent');
+  // // get doc(): AbstractControl {
+  // //   return this.Editorform.get('editorContent');
+  // // }
+
+  // ngOnInit(): void {
+  //   this.editor = new Editor();
   // }
 
-  ngOnInit(): void {
-    this.editor = new Editor();
-  }
-
-  ngOnDestroy(): void {
-    this.editor.destroy();
-  }
+  // ngOnDestroy(): void {
+  //   this.editor.destroy();
+  // }
 }
