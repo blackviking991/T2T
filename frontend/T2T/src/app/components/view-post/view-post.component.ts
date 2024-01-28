1bf27b16fa07e607fea55c65d489b5379203bca3
Atharv Bakre <atharvbakre18@cse.iiitp.ac.in>
2024-01-28 22:00:36 +0530

    View post

--- frontend/T2T/src/app/components/view-post/view-post.component.ts
+++ frontend/T2T/src/app/components/view-post/view-post.component.ts
@@ -2,11 +2,13 @@ import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Comment } from '../../model/Comment';
 import { CommentsSectionComponent } from '../comments-section/comments-section.component';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { Post } from '../../model/Post';
 
 @Component({
   selector: 'app-view-post',
   standalone: true,
-  imports: [CommentsSectionComponent],
+  imports: [NavbarComponent, CommentsSectionComponent],
   templateUrl: './view-post.component.html',
   styleUrl: './view-post.component.scss'
 })
@@ -14,11 +16,24 @@ export class ViewPostComponent {
 
   route: ActivatedRoute = inject(ActivatedRoute);
   postId: number; 
+  post: Post;
   comments: Comment[];
 
   constructor() {
     this.postId = Number(this.route.snapshot.params['id']);
     // dummy data, make API call
+    this.post = {
+      id: 1,
+      title: "Title",
+      desc: "DescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescp",
+      createdBy: "Test User",
+      createdAt: new Date(),
+      tags: ["tag1", "tag1", "tagsdfadfs"],
+      likes: 5,
+      commentIds: [1, 2, 3],
+      forumTag: 8,
+      views: 50
+    }
     let comment: Comment = {
       id: 1,
       desc: "Take me home!Take me home!Take me home!Take me home!Take me home!Take me home!Take me home!Take me home!Take me home!Take me home!Take me home!Take me home!",
