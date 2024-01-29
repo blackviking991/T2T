e4b3b2467097125438a063a17686bdbf51b6003a
blackviking991 <ishaanrock138@gmail.com>
2024-01-29 05:46:00 +0530

    added editor and read post component

--- frontend/T2T/src/app/app.config.ts
+++ frontend/T2T/src/app/app.config.ts
@@ -1,10 +1,10 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { EditorModule, TINYMCE_SCRIPT_SRC  } from '@tinymce/tinymce-angular'
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimations(), provideAnimations()]
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimations(), provideAnimations(), { provide: TINYMCE_SCRIPT_SRC, useValue: 'tinymce/tinymce.min.js' }]
 };
--- frontend/T2T/src/app/components/create-post/create-post.component.ts
+++ frontend/T2T/src/app/components/create-post/create-post.component.ts
@@ -1,46 +1,43 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-// import { Validators, Editor, Toolbar, NgxEditorModule } from 'ngx-editor';
-
-import jsonDoc from './doc';
+import { Component, OnInit } from '@angular/core';
+import { EditorModule } from '@tinymce/tinymce-angular'
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButton } from '@angular/material/button';
+import { ChipsModule } from 'primeng/chips';
 @Component({
   selector: 'app-create-post',
   standalone: true,
-  imports: [ ReactiveFormsModule],
+  imports: [EditorModule, ReactiveFormsModule, MatInputModule, MatButton, ChipsModule],
   templateUrl: './create-post.component.html',
   styleUrl: './create-post.component.scss'
 })
-export class CreatePostComponent {
-  // editordoc = jsonDoc;
-
-  // editor!: Editor;
-  // toolbar: Toolbar = [
-  //   ['bold', 'italic'],
-  //   ['underline', 'strike'],
-  //   ['code', 'blockquote'],
-  //   ['ordered_list', 'bullet_list'],
-  //   [{ heading: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }],
-  //   ['link', 'image'],
-  //   ['text_color', 'background_color'],
-  //   ['align_left', 'align_center', 'align_right', 'align_justify'],
-  // ];
-
-  // Editorform = new FormGroup({
-  //   editorContent: new FormControl(
-  //     { value: jsonDoc, disabled: false },
-  //     Validators.required()
-  //   ),
-  // });
-
-  // // get doc(): AbstractControl {
-  // //   return this.Editorform.get('editorContent');
-  // // }
-
-  // ngOnInit(): void {
-  //   this.editor = new Editor();
-  // }
-
-  // ngOnDestroy(): void {
-  //   this.editor.destroy();
-  // }
+export class CreatePostComponent implements OnInit {
+  addPostForm!: FormGroup;
+  title = new FormControl('');
+  desc = new FormControl('');
+  body = new FormControl('');
+  values = new FormControl<string[] | null>(null);
+  editorConfig = {
+    base_url: '/tinymce',
+    suffix: '.min',
+    plugins: 'lists link image code table wordcount save',
+    height: 650
+  };
+  ngOnInit(): void {
+    this.addPostForm = new FormGroup({
+      title: this.title,
+      desc: this.desc,
+      values: this.values,
+      body: this.body
+    });
+  }
+  addPost() {
+    var obj: any= {};
+    obj['content'] = this.addPostForm.get('body')!.value;
+    console.log(obj);
+    console.log(this.addPostForm.get('body')!.value);
+    console.log(this.addPostForm.get('title')!.value);
+    console.log(this.addPostForm.get('desc')!.value);
+    console.log(this.addPostForm.get('values')!.value);
+  }
 }
