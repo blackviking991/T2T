f3c6cad31408702fa937d8f304147dd1365c2709
Atharv Bakre <atharvbakre18@cse.iiitp.ac.in>
2024-01-29 14:02:21 +0530

    Get for forum & view post

--- frontend/T2T/src/app/components/view-post/view-post.component.ts
+++ frontend/T2T/src/app/components/view-post/view-post.component.ts
@@ -7,11 +7,12 @@ import { Comment } from '../../model/Comment';
 import { CommentsSectionComponent } from '../comments-section/comments-section.component';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { Post } from '../../model/Post';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-view-post',
   standalone: true,
-  imports: [MatIconModule, EditorModule, MatChipsModule,NavbarComponent, CommentsSectionComponent],
+  imports: [HttpClientModule, MatIconModule, EditorModule, MatChipsModule, NavbarComponent, CommentsSectionComponent],
   templateUrl: './view-post.component.html',
   styleUrl: './view-post.component.scss'
 })
@@ -28,34 +29,21 @@ export class ViewPostComponent {
   initialVal = '<h1><img style=\"display: block; margin-left: auto; margin-right: auto;\" src=\"https://miro.medium.com/v2/resize:fit:1400/format:webp/1*1HXCJCOpzKdmQI33ZrEIlg.png\" alt=\"\" width=\"716\" height=\"224\">Angular 17</h1>\n<p><span style=\"font-family: helvetica, arial, sans-serif;\">Angular version 17 released on November 8, 2023 and the hype is real! There are so many things to talk about and get your head around to create modern web apps and updating your existing Angular apps. You can read about the details of each of the new changes on the&nbsp;<a class=\"af nx\" href=\"https://goo.gle/angular-dot-dev\" target=\"_blank\" rel=\"noopener ugc nofollow\">Angular blog post</a>, but lets talk about why these changes are important to you as an Angular developer.</span></p>\n<h2><span style=\"font-family: helvetica, arial, sans-serif;\">New features</span></h2>\n<p id=\"170e\" class=\"pw-post-body-paragraph mz na gr nb b nc nd ne nf ng nh ni nj nk nl nm nn no np nq nr ns nt nu nv nw gk bj\" data-selectable-paragraph=\"\">Before diving in, lets take a look at all the new things in v17&hellip;</p>\n<ul class=\"\">\n<li id=\"0f1d\" class=\"mz na gr nb b nc nd ne nf ng nh ni nj nk nl nm nn no np nq nr ns nt nu nv nw ny nz oa bj\" data-selectable-paragraph=\"\">logo redesign</li>\n<li id=\"c23a\" class=\"mz na gr nb b nc ob ne nf ng oc ni nj nk od nm nn no oe nq nr ns of nu nv nw ny nz oa bj\" data-selectable-paragraph=\"\">website moving from&nbsp;<a class=\"af nx\" href=\"https://angular.io/\" target=\"_blank\" rel=\"noopener ugc nofollow\">angular.io</a>&nbsp;to&nbsp;<a class=\"af nx\" href=\"https://angular.dev/\" target=\"_blank\" rel=\"noopener ugc nofollow\">angular.dev</a></li>\n<li id=\"fa54\" class=\"mz na gr nb b nc ob ne nf ng oc ni nj nk od nm nn no oe nq nr ns of nu nv nw ny nz oa bj\" data-selectable-paragraph=\"\">standalone applications are now the default for new applications</li>\n<li id=\"b63d\" class=\"mz na gr nb b nc ob ne nf ng oc ni nj nk od nm nn no oe nq nr ns of nu nv nw ny nz oa bj\" data-selectable-paragraph=\"\">signals are out of dev preview and are now stable</li>\n<li id=\"8563\" class=\"mz na gr nb b nc ob ne nf ng oc ni nj nk od nm nn no oe nq nr ns of nu nv nw ny nz oa bj\" data-selectable-paragraph=\"\">server-side rendering improvements</li>\n<li id=\"e34e\" class=\"mz na gr nb b nc ob ne nf ng oc ni nj nk od nm nn no oe nq nr ns of nu nv nw ny nz oa bj\" data-selectable-paragraph=\"\">hydration is out of dev preview and is now stable</li>\n<li id=\"a8b0\" class=\"mz na gr nb b nc ob ne nf ng oc ni nj nk od nm nn no oe nq nr ns of nu nv nw ny nz oa bj\" data-selectable-paragraph=\"\">control flow (<code class=\"cw og oh oi oj b\">@if</code>,&nbsp;<code class=\"cw og oh oi oj b\">@for</code>,&nbsp;<code class=\"cw og oh oi oj b\">@switch</code>) *developer preview</li>\n<li id=\"9732\" class=\"mz na gr nb b nc ob ne nf ng oc ni nj nk od nm nn no oe nq nr ns of nu nv nw ny nz oa bj\" data-selectable-paragraph=\"\">defer&nbsp;<em class=\"ok\">(just going to leave this here because justice can not be paid this briefly)</em> *developer preview</li>\n</ul>"';
   route: ActivatedRoute = inject(ActivatedRoute);
   postId: number; 
-  post: Post;
-  comments: Comment[];
+  post!: Post;
 
-  constructor() {
+  constructor(private http: HttpClient) {
     this.postId = Number(this.route.snapshot.params['id']);
-    // dummy data, make API call
-    this.post = {
-      pID: "1",
-      title: "Title",
-      desc: "DescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescpDescp",
-      shortDesc: "Short Desc",
-      createdBy: "Test User",
-      createdTime: new Date(),
-      tags: ["tag1", "tag1", "tagsdfadfs"],
-      likes: 5,
-      forumName: "8",
-      views: 50
-    }
-    let comment: Comment = {
-      cID: "1",
-      text: "Take me home!Take me home!Take me home!Take me home!Take me home!Take me home!Take me home!Take me home!Take me home!Take me home!Take me home!Take me home!",
-      likes: 5,
-      postId: 3,
-      createdBy: "Test User",
-      createdTime: new Date()
-    }
-    comment.childComments = [comment, { ...comment, cID: "2"}, { ...comment, cID: "3"}];
-    this.comments = [comment, { ...comment, cID: "2"}, { ...comment, cID: "3"}];
+    // fetch post by ID
+    this.http.get("http://localhost:8080/forum/" + this.postId, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem("access_token")}`
+      }
+    }).subscribe((res: any) => {
+      this.post = res.posts;
+    }, (err => {
+      console.log("Error fetching post details for id " + this.postId);
+    }));
   }
 
 }
